6ec8255c813ba00e60c7923e33f36efba34cc6e0
PoojithaAlam <poojithaalam@gmail.com>
2024-01-31 13:10:34 -0800

    MAP API

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Map = ({ address }) => {
   const formattedAddress = address.replace(/ /g, '+');
-  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyBlCxpBxsL62WJ69Xpx8bY3TA_8fODvlYY&q=${formattedAddress}`;
+  const mapUrl = `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_MAP_API_KEY}&q=${formattedAddress}`;
   
-
   return (
     <iframe
       style={{ border: 0, width: "30%" }}
