28b773612002156684377065274a8ad3e4a71edd
euiri <57123626+euiri@users.noreply.github.com>
2024-02-07 11:01:47 -0800

    redux initiation, eventList, Loader, Message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,29 +1,41 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import Event from '../components/Event'
+import React, {useEffect} from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { listEvents } from '../actions/eventActions'
+import Event from '../components/Event'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 
 const HomeScreen = () => {
-  const [events, setEvents] = useState([])
+
+  const dispatch = useDispatch()
+
   useEffect( () => {
-    const fetchEvents = async() => {
-      const {data} = await axios.get('/api/events')
-      setEvents(data)
-    }
-    fetchEvents()    
-  })
+    dispatch(listEvents())    
+    }, [dispatch])
+
+  const eventList = useSelector( (state) => state.eventList)
+  const { loading, events, error } = eventList
+
   return (
     <>
       <h1>Best Events and Things to Do</h1>
-      <Row>
-        {events.map(p => (
-          <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
-            <Event event={p} />
-          </Col>
-          ))
-        }
-      </Row>
+      {
+        loading ? (<Loader />)
+          : error ? (<Message variant='danger'> {error}</Message>)
+            : 
+              <Row>
+                {events.map(p => (
+                  <Col key={p._id} sm={12} md={6} lg={4} xl={3}>
+                    <Event event={p} />
+                  </Col>
+                  ))
+                }
+            </Row>
+      
+      }
+      
     </>
   )
 }
---
+++ frontend/src/store.js
@@ -0,0 +1,13 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { eventListReducer } from './reducers/eventReducers'
+
+const rootReducer = combineReducers ({
+  eventList: eventListReducer
+})
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: {}
+})
+
+export default store
