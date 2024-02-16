213a2e050a428eab8fa152c9b7de98743164d55a
PoojithaAlam <poojithaalam@gmail.com>
2024-02-15 23:59:01 -0800

    backend: event controller, user backend(controller, token, routes)

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,21 +1,19 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Event from '../models/eventModel.js'
+import { getEvents,getEventById } from '../Controllers/eventController.js'
+
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async (req, res) => {
-  const events = await Event.find({})
-  res.json(events)
-}))
 
-router.get('/:id', asyncHandler(async (req, res) => {
-  const event = await Event.findById(req.params.id)
-  if(event){
-    res.json(event)
-  } else {
-    res.status(404).json({message: 'Event not found'})
-  }
-}))
+//@desc Fetch all events
+//@route GET/api/events/
+//@access public
+router.get('/',getEvents)
+
+//@desc Fetch single event by id
+//@route GET/api/events/:id
+//@access public
+
+router.get('/:id', getEventById)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,14 @@ import eventRoutes from './routes/eventRoutes.js'
 import mapRoutes from './routes/mapRoutes.js'
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
+app.use(express.json())
+app.use('api/users', userRoutes)
 app.use('/api/events', eventRoutes)
 app.use('/api/map', mapRoutes)
 app.use(errorHandler)
