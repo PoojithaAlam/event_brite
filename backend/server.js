b377cb6d1bd99da2dbb877090770c3dd1dd50150
PoojithaAlam <poojithaalam@gmail.com>
2024-02-27 14:15:32 -0800

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js';
 import errorHandler from './middleware/errorMiddleware.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import path from 'path'
 
 const app = express()
 dotenv.config()
@@ -20,6 +21,15 @@ app.use('/api/events', eventRoutes)
 app.use('/api/map', mapRoutes)
 app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
-app.use(errorHandler)
 
-app.listen(5000, console.log('Server is running on port 5000'))
+const __dirname = path.resolve()
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
+
+app.use(errorHandler)
+const port = process.env.PORT
+app.listen(port, console.log(`Server is running on port ${port}`))
